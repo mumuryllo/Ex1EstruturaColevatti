e6ecdf4ed8cf6573502f0f1510d54b40ad557487
MurylloSoares <MurylloSoares>
2022-08-14 22:28:36 -0300

    Ex1 Fatorial

---
+++ Fatorial/src/Controller.java
@@ -0,0 +1,12 @@
+
+public class Controller {
+
+	
+	public int fatorial(int numero) {
+		if(numero <= 1) {
+			return 1;
+		}
+		return numero * fatorial(numero - 1);
+	}
+	
+}
---
+++ Fatorial/src/View.java
@@ -0,0 +1,11 @@
+
+public class View {
+
+	public static void main(String[] args) {
+     Controller controller= new Controller();
+     
+   int  resultado = controller.fatorial(10);
+   System.out.println(resultado);
+	}
+
+}
